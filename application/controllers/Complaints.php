99c0699e70713db67c63dfa1dbb90b8071cfc63b
Krishna <aryalkrishna642@gmail.com>
2022-03-19 12:17:00 +0630

    Provide base url in config.php

--- application/controllers/Complaints.php
+++ application/controllers/Complaints.php
@@ -30,20 +30,20 @@ class Complaints extends Admin_Controller
 		 if($this->input->post())
 		 {
 
-	     $this->form_validation->set_rules('patient_id', 'Patient Id', 'trim|required');
-		$this->form_validation->set_rules('complaint', 'Complaint', 'trim|required');
+			$this->form_validation->set_rules('patient_id', 'Patient Id', 'trim|required');
+			$this->form_validation->set_rules('complaint', 'Complaint', 'trim|required');
 
 		 if($this->form_validation->run() == TRUE ){	
 
-		 $this->model_complaints->patient_id = $this->input->post('patient_id');
-		 $this->model_complaints->complaint  = $this->input->post('complaint');
-		 $this->model_complaints->save();
-		 $this->model_complaints->load($this->model_complaints->id);
-	     $this->data['complaint'] = $this->model_complaints;
-		 $this->load->view('patients/complaint',$this->data);
+			$this->model_complaints->patient_id = $this->input->post('patient_id');
+			$this->model_complaints->complaint  = $this->input->post('complaint');
+			$this->model_complaints->save();
+			$this->model_complaints->load($this->model_complaints->id);
+			$this->data['complaint'] = $this->model_complaints;
+			$this->load->view('patients/complaint',$this->data);
 		 }else{
 
-		 	echo "'Complaint cannot be empty'";
+		 	// echo "Complaint cannot be empty";
 		 }
 
 	}
